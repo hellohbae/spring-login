5720e8f1e3e21babe04b7ce122b6ef5de6448da8
user-PC\user <wert7871@gmail.com>
2020-07-04 22:24:22 +0900

    second day commit on IJ

--- src/main/java/com/hansam/spring/domain/MemberRepository.java
+++ src/main/java/com/hansam/spring/domain/MemberRepository.java
@@ -1,5 +1,11 @@
 package com.hansam.spring.domain;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hansam.spring.domain.entity.MemberEntity;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 	Optional<MemberEntity> findByEmail(String userEmail);
 }
